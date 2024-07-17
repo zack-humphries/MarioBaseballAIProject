912cf6068ed8c03922bff310bcab2f9334163824
zack-humphries <zhumphries0@gmail.com>
2024-07-17 00:57:10 -0500

    Starting to initialize characters

--- api/characters.py
+++ api/characters.py
@@ -1,4 +1,8 @@
 import memory_values
+from memory_values import *
+import csv
+import os
+
 
 class AllCharacters:
     def __init__(self):
@@ -8,12 +12,10 @@ class AllCharacters:
         self.list.append(character)
 
 
-
-
 class Character:
-    def __init__(self):
-        self.id = None
-        self.name = None
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
         self.stats = None
         self.attributes = None
 
@@ -43,38 +45,94 @@ class Stat(CharacterStats):
 
     #def turn pow2 to list?
         
+def returnFilePath(filename):
+    scriptdir = os.path.dirname(os.path.abspath(__file__))
+    filepath = os.path.join(scriptdir, filename)
+    return filepath
 
     
+def initializeCharacters():
+
+    allcharacters = AllCharacters()
+
+    filepath = returnFilePath("character_stats.csv")
+
+    with open(filepath, 'r') as csvfile:
+
+        csvreader = csv.reader(csvfile)
+
+        fields = list(next(csvreader))
+        datatype = list(next(csvreader))
+        data = list(csvreader)
+
+        print(fields)
+
+        for characterid in memory_values.CharacterID:
+
+            datarow = data[characterid.value]
+
+            currcharacter = Character(characterid.value, characterid.name)
+
+            characterstats = CharacterStats()
+
+            count = 0
+            for statindex in range(1, len(fields)-1):
+
+                if datatype[statindex] != '':
+                    temptype = eval(datatype[statindex])
+                    tempvalue = temptype(int(datarow[statindex]))
+                    characterstats.add_stat(Stat(fields[statindex], tempvalue, type=temptype))
+                count += 1
+
+            currcharacter.stats = characterstats.stats
+
+            allcharacters.add_character(currcharacter)
+
+    return allcharacters
+
+        
+def main():
+    all_characters = initializeCharacters()
+
+    for character in all_characters.list:
+        print(str(character.id) + " " + str(character.name))
+
+        for stat in character.stats:
+            print(stat.name + ": " + str(stat.value) + " <- " + str(stat.type))
+
+        return
 
     
-# class CharacterStats(Character):
-#     def __init__(self):
-#         self.fielding_arm = None
-#         self.batting_stance = None
-#         self.character_class = None
-#         self.weight_class = None
-#         self.is_captain = None
-#         self.curve_ball_speed = None
-#         self.fast_ball_speed = None
-#         self.cursed_ball = None
-#         self.curve = None
-#         self.curve_control = None
-#         self.fielding_ability1 = None
-#         self.fielding_ability2 = None
-#         self.slap_contact_mult = None
-#         self.charge_contact_mult = None
-#         self.slap_power = None
-#         self.charge_power = None
-#         self.bunting = None
-#         self.hit_trajectory_X = None
-#         self.hit_trajectory_Y = None
-#         self.speed = None
-#         self.throwing_arm = None
-#         self.captain_stars = None
-#         self.star_swing = None
-#         self.star_pitch = None
-#         self.chemestry = None
+if __name__ == '__main__':
+    main()
 
+#    stat_list = [
+#         "fielding_arm",
+#         "batting_stance",
+#         "character_class",
+#         "weight_class",
+#         "is_captain",
+#         "curve_ball_speed",
+#         "fast_ball_speed",
+#         "cursed_ball",
+#         "curve",
+#         "curve_control",
+#         "fielding_ability1",
+#         "fielding_ability2",
+#         "slap_contact_mult",
+#         "charge_contact_mult",
+#         "slap_power",
+#         "charge_power",
+#         "bunting",
+#         "hit_trajectory_X",
+#         "hit_trajectory_Y",
+#         "speed",
+#         "throwing_arm",
+#         "captain_stars",
+#         "star_swing",
+#         "star_pitch",
+#         "chemestry"
+#     ]
 # class Chemestry(CharacterStats):
 #     def __init__(self):
         
