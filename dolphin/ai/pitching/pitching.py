13d3da9fd02374c56e62379a824b947a1861912c
zack-humphries <zhumphries0@gmail.com>
2024-08-05 23:28:04 -0500

    adding more to pitching calculations

--- dolphin/ai/pitching/pitching.py
+++ dolphin/ai/pitching/pitching.py
@@ -4,6 +4,14 @@ from api.characters import initializeCharacters
 
 dolphin = Dolphin()
 
+# pitchAttributes = {
+#     "curveball_charge": {
+
+#     } 
+# }
+# do later
+
+
 class Position():
     def __init__(self):
         self.X = None
@@ -21,6 +29,11 @@ def Velocity():
         self.Y = None
         self.Z = None
 
+    def initialize_velocity(self, release_position: Position, target_position: Position, character: ):
+        self.X = release_position.X - target_position.X
+        self.Y = release_position.Y - target_position.Y
+        self.Z = (character.stats['curve_ball_speed'].value)/-240
+
     def calculateVelocity(self, currFrameBall, curveInput):
         self.X = None # something with curve input, curve speed, and curve control
         self.Y = None # something dependent on pitch speed, air resistance, and other stuff
@@ -31,11 +44,23 @@ def Velocity():
         self.Y = currFrameBall.position.Y - prevFrameBall.position.Y
         self.Z = currFrameBall.position.Z - prevFrameBall.position.Z
 
+    def applyAirResistance(frame):
+        airResistanceVelocityAdjustment = 15
+        airResistanceDelay = 20
+
+        
+
+
+
 
 def CurveInput():
     def __init__(self):
         # need to research more
         self.input = None
+
+    def move_pitch(self, direction):
+        pass
+
 
 class Ball():
     def __init__(self):
@@ -43,27 +68,38 @@ class Ball():
         self.velocity = None
         self.curveInput = None
 
+
+    def initialize_velocity(self, release_position, target_position):
+        pass
+
+
     def updateBall(self, lastBall):
+        pass
         
-
-
 class Pitch():
     def __init__(self, memoryAddresses):
         self.memoryAddresses = memoryAddresses
         self.ball = None
         self.mound = None
         self.pitchType = None
+        self.type = None
         self.frame = None
 
     def getFrame(self):
         self.frame = self.memoryAddresses['pitch_frame'].read()
         return self.frame
 
+    def pitchCurve(pitcher, ):
+        pitcher_stats = pitcher.stats
 
-        
-        
-
-
+    def isStrike(self):
+        if (0.5 <= self.ball.position.Z <= 1.05):
+            if (-0.53 <= self.ball.position.X <= 0.53):
+                return True
+            else:
+                return False
+        else:
+            return None
 
 class Pitching():
 
@@ -87,10 +123,15 @@ class Pitching():
     def updatePitchType(self):
         self.pitchType = self.memoryAddresses['pitch_type'].read()
         return self.pitchType
+    
+
 
     
 
-    def move_pitch(self, direction):
-        #blah
+        
+        
+
+
+
 
 
--- dolphin/ai/pitching/pitching_calculations.py
+++ dolphin/ai/pitching/pitching_calculations.py
@@ -1,2 +1,7 @@
 ## here is where I include the math behind Nuche's code from ...
 ## https://github.com/ProjectRio/ProjectRio-frontend/blob/main/src/lib/helpers/MssbFunctions/PitchSimulation/pitchCurve.ts
+
+from helper_functions import linearInterpolateToNewRange
+
+
+
